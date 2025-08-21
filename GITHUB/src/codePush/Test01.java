fc8cf156f5e215c6d3e0087fc5ccefb60f45353e
Chinmoy Dhabal <chinmoydhabal@gmail.com>
2025-08-21 20:44:31 +0530

    initial test commit

---
+++ GITHUB/src/codePush/Test01.java
@@ -0,0 +1,8 @@
+package codePush;
+
+public class Test01 {
+	public static void main(String args[]) {
+		System.out.println("Pushing code to the remote repository.");
+		System.out.println("Code pushed successfully!");
+	}
+}
